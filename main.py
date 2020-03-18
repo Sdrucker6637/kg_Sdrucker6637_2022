655590c3d703fd9b7f60f9453dd0a092fc315b1e
Sdrucker6637 <62318485+Sdrucker6637@users.noreply.github.com>
2020-03-18 16:03:40 -0400

    Added variables for length of s1 and s2
    Did this to make internal code in if and for statements slightly less wordy and confusing.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def mapchecker(s1,s2):
-    if len(s1)<= len(s2):
+    s1s=len(s1)
+    s2s=len(s2)
+    if s1s<= s2s:
         dr={}
-        for i in range(len(s1)):
+        for i in range(s1s):
             if s1[i] not in dr:
                 dr[s1[i]]=s2[i]
             if dr[s1[i]]!=s2[i]:
